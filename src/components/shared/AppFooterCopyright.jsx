b54e5318d2ce8f6ab4cbc7dbcb3ef8ace6f76509
Chanaka Abeysinghe <chanaka6775@gmail.com>
2023-04-03 01:33:16 +0000

    update footer

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -2,19 +2,19 @@ function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-				&copy; {new Date().getFullYear()}
+				&nbsp; &copy; {new Date().getFullYear()}
 				<a
 					href="https://github.com/realstoman/react-tailwindcss-portfolio"
 					target="__blank"
-					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+					className="text-secondary-dark dark:text-secondary-light font-medium hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
-					React & Tailwind CSS Portfolio
+					Created using a template
 				</a>
 				.
 				<a
 					href="https://stoman.me"
 					target="__blank"
-					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+					className="uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
 					Stoman
 				</a>
